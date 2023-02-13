1a677f044f7afb4a1399bd869966884e3ab10088
Idoma Ngbede <idomakevin@gmail.com>
2023-02-13 01:13:38 +0100

    creating and updating directorypage with directory component and category item component

--- src/components/category-item/category-item.js
+++ src/components/category-item/category-item.js
@@ -1,18 +1,20 @@
 import './category-item.scss';
 
-const CategoryItem = ({ category }) => {
-  const { title, imageUrl } = category;
+const CategoryItem = ({ category: { title, imageUrl } }) => {
   return (
-    <div className='category-container'>
-      <div
-        className='background-image'
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
-      <div className='category-body-container'>
-        <h2>{title}</h2>
-        <p>Shop Now</p>
+    <>
+      <div className='category-container'>
+        <div
+          className='background-image'
+          style={{ backgroundImage: `url(${imageUrl})` }}
+        />
+
+        <div className='category-body-container'>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -1,13 +1,15 @@
-import CategoryItem from '../category-item/category-item';
 import './directory.scss';
+import CategoryItem from '../category-item/category-item';
 
 const Directory = ({ categories }) => {
   return (
-    <div className='directory-container'>
-      {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
-      ))}
-    </div>
+    <>
+      <div className='directory-container'>
+        {categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
+        ))}
+      </div>
+    </>
   );
 };
 
